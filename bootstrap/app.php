eb37571c0730cbcb5579929ec914a916a47932f0
muhsirojulmunir <munirxanm40@gmail.com>
2026-05-18 11:36:48 +0700

    Remove Vercel configuration files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,10 +16,3 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
-
-// Use /tmp/storage on Vercel serverless (filesystem is read-only)
-if ($storagePath = env('APP_STORAGE')) {
-    $app->useStoragePath($storagePath);
-}
-
-return $app;
