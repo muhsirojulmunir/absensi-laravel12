be36ce564aca99cfb92f42c27cf38c2e557b0ea7
muhsirojulmunir <munirxanm40@gmail.com>
2026-05-18 11:09:40 +0700

    Fix routing and simplify test

--- api/test.php
+++ api/test.php
@@ -1,13 +1,3 @@
 <?php
 
-header('Content-Type: application/json');
-
-echo json_encode([
-    'status' => 'OK',
-    'php_version' => phpversion(),
-    'message' => 'PHP berhasil berjalan di Vercel!',
-    'extensions' => get_loaded_extensions(),
-    'vendor_exists' => file_exists(__DIR__ . '/../vendor/autoload.php'),
-    'composer_exists' => file_exists(__DIR__ . '/../composer.json'),
-    'public_exists' => file_exists(__DIR__ . '/../public/index.php'),
-], JSON_PRETTY_PRINT);
+echo "PHP berjalan! Versi: " . phpversion();
