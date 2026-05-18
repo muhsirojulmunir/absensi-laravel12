cb49a5be2292ab6954a6567c4f15c9a3d842b7ba
muhsirojulmunir <munirxanm40@gmail.com>
2026-05-18 11:17:16 +0700

    Configure Laravel to use /tmp storage on Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,8 +4,14 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
+$app = Application::configure(basePath: dirname(__DIR__));
+
+// Use /tmp/storage on Vercel serverless (filesystem is read-only)
+if ($storagePath = env('APP_STORAGE')) {
+    $app->useStoragePath($storagePath);
+}
+
+$app->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
